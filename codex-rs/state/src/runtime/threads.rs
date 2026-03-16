13438780e7e32eed9d15ffc916f5553dbe3096d6
SDGLBL <lijieisguge@gmail.com>
2026-03-17 06:30:00 +0800

    feat: preserve wire session ids across resumed forks
    Change-Id: I7617fc308349acbefe36a1e9bb6caa78dfa686ed
    (cherry picked from commit 11b2f085cba68228807562fd2524c092fa14dbac)
    (cherry picked from commit cdd5197e30820cb82c8228928480f8611b5b78b7)
    (cherry picked from commit 5977b9ba15aca93fc9808c38dd7b8d0aa683f624)
    (cherry picked from commit a66f0a36801b6e299555ae8169bad04f0dedb0eb)
    (cherry picked from commit f1d427e7ca7df77fce03efe9b2926053cb258499)
    (cherry picked from commit 202a5c9e3d73fc7215d9705767f7780ab411703c)

--- codex-rs/state/src/runtime/threads.rs
+++ codex-rs/state/src/runtime/threads.rs
@@ -752,6 +752,7 @@ mod tests {
         let items = vec![RolloutItem::SessionMeta(SessionMetaLine {
             meta: SessionMeta {
                 id: thread_id,
+                wire_session_id: Some(thread_id),
                 forked_from_id: None,
                 timestamp: metadata.created_at.to_rfc3339(),
                 cwd: PathBuf::new(),
@@ -806,6 +807,7 @@ mod tests {
         let items = vec![RolloutItem::SessionMeta(SessionMetaLine {
             meta: SessionMeta {
                 id: thread_id,
+                wire_session_id: Some(thread_id),
                 forked_from_id: None,
                 timestamp: created_at,
                 cwd: PathBuf::new(),
