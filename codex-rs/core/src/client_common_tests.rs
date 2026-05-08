4179986bbfabc9b6bc2a557afbe01c8c95f268d1
李杰 <lijie.20@bytedance.com>
2026-05-08 19:34:39 +0800

    feat: allow model max output token overrides
    (cherry picked from commit 7714f4b1224794c4964089eebea26ec391891777)
    (cherry picked from commit 83669b3bf3958d818b35b53bdff8916885077a9b)
    (cherry picked from commit 4f0390f86a17bfa099cf978ce3e6a87095403ea2)
    (cherry picked from commit 55c12f56909f6314c603e4a1cc8231c7287e68a5)
    (cherry picked from commit ece9b5a22cb99e8a2052e53395bd1b3390e43ca9)
    (cherry picked from commit f805cb81e4cfabee6841b1d6c3cd002a268f1aac)
    (cherry picked from commit f4cb35437c00ced049d39981678bd136376cee36)
    (cherry picked from commit 74c6b97529cb8140533aa652607ef3068742c5d3)
    (cherry picked from commit 305b014b874b0546c5aa75616fc900fba51cd5b4)
    (cherry picked from commit 24d4076aaaca27fdd8d531e32278a48893f543a9)

--- codex-rs/core/src/client_common_tests.rs
+++ codex-rs/core/src/client_common_tests.rs
@@ -109,6 +109,7 @@ fn serializes_text_verbosity_when_set() {
         include: vec![],
         prompt_cache_key: None,
         service_tier: None,
+        max_output_tokens: None,
         text: Some(TextControls {
             verbosity: Some(OpenAiVerbosity::Low),
             format: None,
@@ -156,6 +157,7 @@ fn serializes_text_schema_with_strict_format() {
         include: vec![],
         prompt_cache_key: None,
         service_tier: None,
+        max_output_tokens: None,
         text: Some(text_controls),
         client_metadata: None,
     };
@@ -217,6 +219,7 @@ fn omits_text_when_not_set() {
         include: vec![],
         prompt_cache_key: None,
         service_tier: None,
+        max_output_tokens: None,
         text: None,
         client_metadata: None,
     };
@@ -240,6 +243,7 @@ fn serializes_flex_service_tier_when_set() {
         include: vec![],
         prompt_cache_key: None,
         service_tier: Some(ServiceTier::Flex.to_string()),
+        max_output_tokens: None,
         text: None,
         client_metadata: None,
     };
