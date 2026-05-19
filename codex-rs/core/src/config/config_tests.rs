10ce7ac77370e9fab52f665448c35555cbece312
李杰 <lijie.20@bytedance.com>
2026-05-19 13:48:13 +0800

    fix: adapt candidate 132 test fallout
    (cherry picked from commit a45e47e29ea427d51f161799559f7ea6a9e7e91f)
    (cherry picked from commit 0d663ecbfcb818af5d860460c67d4262f959d791)
    (cherry picked from commit 54da1141ee2b2f6c0fc86c7a646be52f14efab47)
    (cherry picked from commit 41c2932bb9ed422a253833fd3a4e11c441e81128)
    (cherry picked from commit 1cad366e8a94c9742add453624ad99cd4734314a)
    (cherry picked from commit f2b3638ec3f75664adb866b84adf578f595c007d)
    (cherry picked from commit 8c055bfbd9635dd2bd44f3b88e4a501e2d0f766e)
    (cherry picked from commit 4408b09b04b7322b6fcac24cfd2b635c7d3418aa)

--- codex-rs/core/src/config/config_tests.rs
+++ codex-rs/core/src/config/config_tests.rs
@@ -9953,7 +9953,7 @@ min_wait_timeout_ms = -1
         codex_home.path().join(CONFIG_TOML_FILE),
         r#"[features.multi_agent_v2]
 enabled = true
-min_wait_timeout_ms = 3600001
+min_wait_timeout_ms = 14400001
 "#,
     )?;
 
@@ -9967,7 +9967,7 @@ min_wait_timeout_ms = 3600001
     assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
     assert_eq!(
         err.to_string(),
-        "features.multi_agent_v2.min_wait_timeout_ms must be at most 3600000"
+        "features.multi_agent_v2.min_wait_timeout_ms must be at most 14400000"
     );
 
     std::fs::write(
@@ -9995,7 +9995,7 @@ max_wait_timeout_ms = -1
         codex_home.path().join(CONFIG_TOML_FILE),
         r#"[features.multi_agent_v2]
 enabled = true
-max_wait_timeout_ms = 3600001
+max_wait_timeout_ms = 14400001
 "#,
     )?;
 
@@ -10009,7 +10009,7 @@ max_wait_timeout_ms = 3600001
     assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
     assert_eq!(
         err.to_string(),
-        "features.multi_agent_v2.max_wait_timeout_ms must be at most 3600000"
+        "features.multi_agent_v2.max_wait_timeout_ms must be at most 14400000"
     );
 
     std::fs::write(
