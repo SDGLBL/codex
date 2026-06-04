d518e331163f747ac5c7cd4e04b7f5b1db23993f
李杰 <lijie.20@bytedance.com>
2026-06-04 13:09:07 +0800

    fix: adapt candidate 137 replay fallout
    (cherry picked from commit 63f3dbd6d0b2e35b3915976b2741a086d84fed2a)

--- codex-rs/app-server/src/request_processors/thread_processor.rs
+++ codex-rs/app-server/src/request_processors/thread_processor.rs
@@ -2731,6 +2731,17 @@ impl ThreadRequestProcessor {
                 )));
             }
             None
+        } else if let Ok(existing_thread_id) = ThreadId::from_string(&params.thread_id)
+            && let Ok(existing_thread) = self.thread_manager.get_thread(existing_thread_id).await
+        {
+            let source_thread = self
+                .read_stored_thread_for_resume(
+                    &params.thread_id,
+                    /*path*/ None,
+                    /*include_history*/ true,
+                )
+                .await?;
+            Some((existing_thread_id, existing_thread, source_thread))
         } else if params.path.is_some() {
             let source_thread = self
                 .read_stored_thread_for_resume(
@@ -2756,17 +2767,6 @@ impl ThreadRequestProcessor {
             } else {
                 None
             }
-        } else if let Ok(existing_thread_id) = ThreadId::from_string(&params.thread_id)
-            && let Ok(existing_thread) = self.thread_manager.get_thread(existing_thread_id).await
-        {
-            let source_thread = self
-                .read_stored_thread_for_resume(
-                    &params.thread_id,
-                    /*path*/ None,
-                    /*include_history*/ true,
-                )
-                .await?;
-            Some((existing_thread_id, existing_thread, source_thread))
         } else {
             let source_thread = self
                 .read_stored_thread_for_resume(
