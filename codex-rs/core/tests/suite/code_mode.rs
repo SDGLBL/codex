72fe82876a9af997061bb513e6f0e269c24829ab
李杰 <lijie.20@bytedance.com>
2026-06-17 20:26:26 +0800

    fix: preserve code-mode exec output budgets
    (cherry picked from commit 7bb512a25e3d17ba9f7360560026565e7995cac8)
    (cherry picked from commit 4295e7c9f2aead4fa132c0d6bf746915768aa05f)
    (cherry picked from commit 865ff6507af29b561ed6b8f18b66d72b9063fa57)
    (cherry picked from commit d23f40795e8274bd9037a65c3f6fc98d30f6ffaf)
    (cherry picked from commit e241d3f43fab6ec346dc1faa759c3a939251bd8e)
    (cherry picked from commit abf756e7ae10c388aa27db363f90a2227c154e33)
    (cherry picked from commit 96de0baf68d447ebdcd609a14f74e1674fa6bcdf)
    (cherry picked from commit c7800bceb62eb659adadd9226f75f02c5ec8603c)
    (cherry picked from commit 2ea362ac2118737a2315fd07f3f91d20f6802df8)
    (cherry picked from commit 7478e6aa90d8b78df5e9fe8deea6352a8dcbfddb)
    (cherry picked from commit e3eb37bc15e966e2a69e47661be46dbe13dac9e7)
    (cherry picked from commit d4e9371e9b109c14d2fafd1f691a5e76101dd99e)
    (cherry picked from commit db1f4290bebb91bfd84992f98220dd3093bf1b79)
    (cherry picked from commit aa2d78a3e399f4bd61e93ef00f82b5d1a1f7d67c)
    (cherry picked from commit a31c781c5b7a4bc71c3262393c8280f53c71d51a)
    (cherry picked from commit ce3e52003d3900dd81f271d26539e57927bc6990)
    (cherry picked from commit 9ba348e2497a99fd843d52524c501afe48c5de6d)
    (cherry picked from commit 1b968d6ef0228a37326c38fc6060fb148e46932e)

--- codex-rs/core/tests/suite/code_mode.rs
+++ codex-rs/core/tests/suite/code_mode.rs
@@ -1401,7 +1401,8 @@ if (!tool) {
             "exec".to_string(),
             "wait".to_string(),
             "request_user_input".to_string(),
-            "web_search".to_string()
+            "web_search".to_string(),
+            "image_generation".to_string(),
         ]
     );
 
