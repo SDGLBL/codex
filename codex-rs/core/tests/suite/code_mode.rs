2ea362ac2118737a2315fd07f3f91d20f6802df8
李杰 <lijie.20@bytedance.com>
2026-06-17 20:26:26 +0800

    fix: preserve code-mode exec output budgets
    (cherry picked from commit 7bb512a25e3d17ba9f7360560026565e7995cac8)
    (cherry picked from commit 4295e7c9f2aead4fa132c0d6bf746915768aa05f)
    (cherry picked from commit 865ff6507af29b561ed6b8f18b66d72b9063fa57)
    (cherry picked from commit d23f40795e8274bd9037a65c3f6fc98d30f6ffaf)
    (cherry picked from commit e241d3f43fab6ec346dc1faa759c3a939251bd8e)
    (cherry picked from commit abf756e7ae10c388aa27db363f90a2227c154e33)
    (cherry picked from commit 96de0baf68d447ebdcd609a14f74e1674fa6bcdf)
    (cherry picked from commit c7800bceb62eb659adadd9226f75f02c5ec8603c)

--- codex-rs/core/tests/suite/code_mode.rs
+++ codex-rs/core/tests/suite/code_mode.rs
@@ -719,7 +719,8 @@ if (!tool) {
             "exec".to_string(),
             "wait".to_string(),
             "request_user_input".to_string(),
-            "web_search".to_string()
+            "web_search".to_string(),
+            "image_generation".to_string(),
         ]
     );
 
