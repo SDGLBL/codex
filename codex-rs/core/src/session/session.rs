3e6d4cce2197bdd48b4628d20e2f0caf6e86ecf2
李杰 <lijie.20@bytedance.com>
2026-06-17 20:26:12 +0800

    feat: support internal model request overrides
    (cherry picked from commit 40bdc90301f75b495f560d6f0f8b8264f7ec67f4)
    (cherry picked from commit 412df50f7073d73253beaa7610db1aa27d2ade99)
    (cherry picked from commit 7db7a90e14603ad6b42385d167697032d6b1004d)
    (cherry picked from commit 0ab4a20cf46ad79316e7e16436c0a005ac5421db)
    (cherry picked from commit 32d5f6f4a9813a26ae279fdaf9c7016f6e7405de)
    (cherry picked from commit b8fd65e3b4ed98bb3db1712db918aa8343a06e85)
    (cherry picked from commit 8cf52aeb626627731a44ef4fe3016be9479c286e)
    (cherry picked from commit 45605aae105cdb0dc56390365e87363e05d8aaea)
    (cherry picked from commit 3c01654b1f22ce2628be16e557967cf67a158c19)
    (cherry picked from commit 75d0b7af5a074e20503827b801b89ac351b69289)
    (cherry picked from commit 8c6ce97cf36d684aadc3c28837ea0c2c0a185ebd)
    (cherry picked from commit f980854622aa9d03cceba1144d31ecbb3bbdc33f)
    (cherry picked from commit b31c7ff11d0f52695ef27dabf3546f3802975509)
    (cherry picked from commit df7cf2ba840e3f6bc4fac4ef14860b7ab200b2b1)
    (cherry picked from commit f720a246b3370a5c0551fe13d2ad957d425ae30b)
    (cherry picked from commit 4de1242927396e1dea008c1143dd75d342a4d1eb)

--- codex-rs/core/src/session/session.rs
+++ codex-rs/core/src/session/session.rs
@@ -1470,6 +1470,7 @@ impl Session {
                     session_configuration.originator.clone(),
                     config.model_verbosity,
                     config.features.enabled(Feature::ContentItemKinds),
+                    config.model_max_output_tokens,
                     config.features.enabled(Feature::EnableRequestCompression),
                     config.features.enabled(Feature::RuntimeMetrics),
                     Self::build_model_client_beta_features_header(config.as_ref()),
