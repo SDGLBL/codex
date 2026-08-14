416e75ea95760b12c36600a0040f0485b9b79122
李杰 <lijie.20@bytedance.com>
2026-08-14 22:42:44 +0800

    chore: adapt internal patch stack to upstream 0.147.0
    (cherry picked from commit a7aa3f59bc962e1b2681cafcdbf794af2987ba1a)
    (cherry picked from commit 1da95207dccb4716122ea589e413dd9658c1b9d3)
    (cherry picked from commit 36d7fc2ad40e1fd1bb856e58f51b70700e863ef9)

--- codex-rs/core/src/thread_manager.rs
+++ codex-rs/core/src/thread_manager.rs
@@ -1935,7 +1935,6 @@ impl ThreadManagerState {
             Some(
                 self.get_thread(*parent_thread_id)
                     .await?
-                    .codex
                     .session
                     .wire_session_id()
                     .await,
